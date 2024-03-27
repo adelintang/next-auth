4ecd753509f7efc1c2fa229f6c383346f270d4ea
adelintang <adelintang157@gmail.com>
2024-03-27 11:46:49 +0700

    perf(page): root and home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,9 @@
+import HeaderHome from "../components/HeaderHome"
+
 const HomePage = () => {
   return (
     <div className="min-h-screen">
-      <header className="bg-[#14213D] flex items-center justify-between p-3">
-        <h1 className="text-white">Username</h1>
-        <button className="text-white bg-red-600 hover:bg-red-700 py-1 px-2 rounded">Logout</button>
-      </header>
+      <HeaderHome />
       <main className="w-full">
         <p className="text-xl text-center mt-6">Welcome to Dashboard</p>
       </main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,38 @@
+'use client';
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    (async () => {
+      const { data, status } =  await getUser()
+
+      if (!status) {
+        router.push('/')
+        setIsSuccess(true);
+        
+        return
+      }
+
+      router.push('/home')
+
+      // if the error did not happen, if everything is alright
+    })()
+  }, [router])
+
+  if (!isSuccess) {
+    return (
+      <div className="min-h-screen flex">
+        <p className="m-auto">Loading...</p>
+      </div>
+    )
+  }
+
   return (
     <main className="w-full h-screen flex">
       <div className="m-auto flex gap-x-2">
@@ -19,4 +51,20 @@ export default function Home() {
       </div>
     </main>
   );
+}
+
+async function getUser () {
+  const response = await fetch('/api/me')
+  const responseJson = await response.json()
+  const { status, data, message } = responseJson
+
+if (!status) {
+  return {
+    status, message
+  }
+}
+
+  return {
+    status, data
+  }
 }
